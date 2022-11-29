c1045cd243a07428440a416496f9bc82b6b637c8
Agustin Black <agustinblack04@gmail.com>
2022-11-29 20:37:33 -0300

    armado de main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+let nombre = prompt("Ingerse su nombre de usuario: ");
+console.log(nombre);
+
+let pronostico = prompt("Ingrese su pronostico: ");
+if (pronostico == "Gana Argentina") {
+    alert ("Sumas 3 puntos");
+}else if (pronostico == "Gana Mexico") {
+    alert ("Resta 1 puntos");
+}else if (pronostico == "Empatan Argentina vs Mexico"){
+    alert ("Sumas 0 puntos");
+}
+console.log(pronostico);
+
+
+let valorUno = parseInt(prompt("Ingrese el primer valor para la suma de puntos:"));
+let valorDos = parseInt(prompt("Ingrese el segundo valor:"));
+let valorTres = parseInt(prompt("Ingrese el tercer valor:"));
+
+let operacion = prompt("Ingrese (+ -) o (- +) o (+ +) o (- -) dependiendo los puntos y orden en que los conseguiste.");
+
+switch(operacion) {
+    case "+ -":
+        console.log(valorUno + valorDos - valorTres + " puntos son los que tiene.");
+        break;
+    case "- +":
+        console.log(valorUno - valorDos + valorTres + " puntos son los que tiene.");
+        break;
+    case "+ +":
+        console.log(valorUno + valorDos + valorTres + " puntos son los que tiene.");
+        break;
+    case "- -":
+        console.log(valorUno - valorDos - valorTres + " puntos son los que tiene.");
+        break;
+    default: 
+    console.log("Ingrese bien los datos");
+    break;
+}
+
+
+
